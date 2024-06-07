c1ee51b5ca68e9fec761d96c57ef35da5eea370d
edgramirez <49081177+edgramirez@users.noreply.github.com>
2024-06-06 22:30:45 -0400

    lib

---
+++ lib/service_variables.py
@@ -0,0 +1,83 @@
+global urls
+urls = {}
+
+global header
+header = None
+
+global action
+action = {}
+
+global scfg
+scfg = {}
+
+global known_face_encodings
+known_face_encodings = {}
+
+global known_face_metadata
+known_face_metadata = {}
+
+global active_ids_per_camera
+active_ids_per_camera = {}
+
+global inactive_ids_per_camera
+inactive_ids_per_camera = {}
+
+global not_applicable_id
+not_applicable_id = {}
+
+global input_file
+input_file = {}
+
+global face_detection_url
+face_detection_url = {}
+
+global delta_time
+delta_time = {}
+
+global service_camera_mac_address
+service_camera_mac_address = {}
+
+global blacklist_encodings
+blacklist_encodings = {}
+
+global blacklist_metas
+blacklist_metas = {}
+
+global whitelist_encodings
+whitelist_encodings = {}
+
+global whitelist_metas
+whitelist_metas = {}
+
+global gender_age_dict
+gender_age_dict = {}
+
+global accumulate_gender_age_dict
+accumulate_gender_age_dict = {}
+
+global genderList
+genderList = []
+
+global ageList
+ageList = []
+
+global treated_ids
+treated_ids = {}
+
+global MODEL_MEAN_VALUES
+MODEL_MEAN_VALUES = ()
+
+global active_service_names
+active_service_names = []
+
+global ids_status
+ids_status = {}
+
+global services_by_camera_id
+services_by_camera_id = {}
+
+global client_name
+client_name = ""
+
+global search_db_name_dict
+search_db_name_dict = {}
